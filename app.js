eeddefcc96838203cc0419a6ed4bc8c61382dd5e
vladkoryoin01 <vladkoryoin@gmail.com>
2025-03-05 17:57:46 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// const firstName = 'Alice'
+// firstName = 'Vlad'
+// console.log('Hello', firstName)
+
+let firstName = 'Vlad'
+firstName = 'Alice'
+console.log('Hi', firstName)
+
+var lastName = 'Kim'
+lastName = 'Mun'
+console.log(lastName)
