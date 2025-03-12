77d317dce3d8c2506ee70db9ccc0c0d6cae809e4
vladkoryoin01 <vladkoryoin@gmail.com>
2025-03-13 00:45:08 +0500

    Переместил Lesson3 в VScode

---
+++ VScode/lesson03.js
@@ -0,0 +1,12 @@
+
+
+
+
+const titleProject = "Оценка проекта";
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+const screenPrice = 5000;
+const percentage = 20;
+const fullPrice = 100000;
+const responsive = true;
+
+
